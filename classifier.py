88d6a35ffac6346bd319c81ddfdb3e7b40d99c45
agarwalchaitanya <00agchaitanya@gmail.com>
2019-04-20 04:19:17 +0530

    minor fix

--- classifier.py
+++ classifier.py
@@ -22,7 +22,7 @@ def classifier(X_train, Y_train, X_test, Y_test):
   tf_model.add(InputLayer(input_shape=[100]))
   tf_model.add(LSTM(200, dropout_U = 0.2, dropout_W = 0.2))
   tf_model.add(Dense(len(set(Y_train)), activation="softmax"))
-  tf_model.compile(loss="catrgorical_crossentropy", optimizer="adam", metrics="['accuracy]")
+  tf_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics="['accuracy']")
   tf_model.fit(X_train, Y_train, batch_size=32, nb_epoch=100, verbose=1)
   score, acc = tf_model.evaluate(X_test, Y_test, verbose=1, batch_size=32)
   print("Score: %.2f" % (score))
@@ -30,8 +30,10 @@ def classifier(X_train, Y_train, X_test, Y_test):
   return tf_model
 
 for acronym, expansions in acr.items():
-  train_data = []
-  test_data = []
+  X_train = []
+  X_test = []
+  Y_train = []
+  Y_test = []
   for expansion in expansions:
     with open("train_data/"+str(expansion)) as file:
       tweets = file.readlines()
@@ -39,12 +41,14 @@ for acronym, expansions in acr.items():
         tweet = json.loads(tweet)
         tweet = tweet.lower()
         tweet = tweet.replace(expansion, acronym)
-        train_data.append([model.infer_vector(tweet.split()), expansion])
+        X_train.append(model.infer_vector(tweet.split()))
+        Y_train.append(expansion)
     with open("test_data/"+str(expansion)+".txt") as file:
       tweets = file.readlines()
       for tweet in tweets:
         tweet = json.loads(tweet)
         tweet = tweet.lower()
         tweet = tweet.replace(expansion, acronym)
-        test_data.append([model.infer_vector(tweet.split()), expansion])
-  classifier(train_data[0], train_data[1], test_data[0], test_data[1]).save(str(acronym)+".h5")
+        X_test.append(model.infer_vector(tweet.split()))
+        Y_test.append(expansion)
+  classifier(X_train, Y_train, X_test, Y_test).save(str(acronym)+".h5")
