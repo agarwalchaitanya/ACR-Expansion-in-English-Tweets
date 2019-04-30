036126d48152aeab0f1c50180f1c00e411829264
agarwalchaitanya <00agchaitanya@gmail.com>
2019-04-30 09:37:56 +0530

    ADD search/classification functionality

--- search/views.py
+++ search/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from django.template import loader
+from acrlist import acr
+import pickle
+import os
+import tensorflow as tf 
+import keras
+from keras.models import load_model
+from keras.utils.np_utils import to_categorical
+import numpy as np
+from ACR.settings import BASE_DIR as base
 
-def index(request):
+# Create your views here.
+d_model = pickle.load(open(base+'/doc2vec.pickle', 'rb'))
+
+
+def index(request):    
+    if request.method == 'POST':
+      search_string = request.POST['search_string']
+      search_string = search_string.lower()
+      for acronym, expansions in acr.items():
+        if acronym in search_string.split():
+          infer_vector = d_model.infer_vector(search_string.split())
+          keras.backend.clear_session()
+          classifier = load_model(base+"/classifiers/"+str(acronym)+".h5")
+          test = np.array(tf.keras.utils.normalize(infer_vector))
+          dist = classifier.predict(test)
+          prediction = classifier.predict_classes(test)
+          prediction_ = expansions[np.asscalar(prediction)]
+          template = loader.get_template('search/index.html')
+          context = {
+            'search_string' : search_string,
+            'prediction' : prediction_,
+            'acronym' : acronym,
+          }
+          return HttpResponse(template.render(context, request))
     return render(request, 'search/index.html')
